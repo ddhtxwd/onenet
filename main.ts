e3d1d91518693afb92ef21c2132f7314269d7c55
ddhtxwd <ddh895647891>
2021-05-28 14:40:48 +0800

    添加TM1650

--- main.ts
+++ main.ts
@@ -208,7 +208,7 @@ namespace OneNET {
      * @param y ; eg: 0
      * @param number ; eg: 666
     */
-    //% block="在屏幕的位置第 $y 行第 $x 列上显示数字: $number"
+    //% block="在屏幕的位置第 $y 行第 $x 列上显示数字: $number"  group="物控盒显示"
     //% subcategory="显示"
     export function lcd_display_number(x: number, y: number, number: number): void {
         let cmd: string = "AT+DRAW=" + convertToText(x) + ',' + convertToText(y+1) + ',' + convertToText(number) + '\n'
@@ -231,7 +231,7 @@ namespace OneNET {
      * @param y ; eg: 0
      * @param string ; eg: "hello world"
     */
-    //% block="在屏幕的位置第 $y 行第 $x 列上显示文本: $string"
+    //% block="在屏幕的位置第 $y 行第 $x 列上显示文本: $string"  group="物控盒显示"
     //% subcategory="显示"
     export function lcd_display_string(x: number, y: number, string: string): void {
         let cmd: string = "AT+DRAW=" + convertToText(x) + ',' + convertToText(y+1) + ',' + string + '\n'
@@ -248,7 +248,7 @@ namespace OneNET {
         basic.pause(100)
     }
 
-    //% block="清除显示"
+    //% block="清除显示"  group="物控盒显示"
     //% subcategory="显示"
     export function lcd_clear(): void {
         let cmd: string = "AT+DRAW=0,1,.Clear.\n"
@@ -264,7 +264,147 @@ namespace OneNET {
         serial.writeString(cmd)
         basic.pause(200)
     }
+	
+	let COMMAND_I2C_ADDRESS = 0x24
+    let DISPLAY_I2C_ADDRESS = 0x34
+    let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
 
+    let _intensity = 3
+    let dbuf = [0, 0, 0, 0]
+
+    /**
+     * send command to display
+     * @param is command, eg: 0
+     */
+    function cmd(c: number) {
+        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
+    }
+
+    /**
+     * send data to display
+     * @param is data, eg: 0
+     */
+    function dat(bit: number, d: number) {
+        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.Int8BE)
+    }
+
+    /**
+     * turn on display
+     */
+    //% blockId="TM650_ON" block="开启显示"  group="TM1650数码管"
+    //% weight=50 blockGap=8
+	//% subcategory="显示"
+    export function on() {
+        cmd(_intensity * 16 + 1)
+    }
+
+    /**
+     * turn off display
+     */
+    //% blockId="TM650_OFF" block="关闭显示"  group="TM1650数码管"
+    //% weight=50 blockGap=8
+	//% subcategory="显示"
+    export function off() {
+        _intensity = 0
+        cmd(0)
+    }
+
+    /**
+     * clear display content
+     */
+    //% blockId="TM650_CLEAR" block="清空显示"  group="TM1650数码管"
+    //% weight=40 blockGap=8
+	//% subcategory="显示"
+    export function clear() {
+        dat(0, 0)
+        dat(1, 0)
+        dat(2, 0)
+        dat(3, 0)
+        dbuf = [0, 0, 0, 0]
+    }
+
+    /**
+     * show a digital in given position
+     * @param digit is number (0-15) will be shown, eg: 1
+     * @param bit is position, eg: 0
+     */
+    //% blockId="TM650_DIGIT" block="显示数字 %num|在 %bit"  group="TM1650数码管"
+    //% weight=80 blockGap=8
+    //% num.max=15 num.min=0
+	//% subcategory="显示"
+    export function digit(num: number, bit: number) {
+        dbuf[bit % 4] = _SEG[num % 16]
+        dat(bit, _SEG[num % 16])
+    }
+
+    /**
+     * show a number in display
+     * @param num is number will be shown, eg: 100
+     */
+    //% blockId="TM650_SHOW_NUMBER" block="显示数字 %num"  group="TM1650数码管"
+    //% weight=100 blockGap=8
+	//% subcategory="显示"
+    export function showNumber(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit(Math.idiv(num, 1000) % 10, 0)
+        digit(num % 10, 3)
+        digit(Math.idiv(num, 10) % 10, 2)
+        digit(Math.idiv(num, 100) % 10, 1)
+    }
+
+    /**
+     * show a number in hex format
+     * @param num is number will be shown, eg: 123
+     */
+    //% blockId="TM650_SHOW_HEX_NUMBER" block="显示16进制数字 %num"  group="TM1650数码管"
+    //% weight=90 blockGap=8
+	//% subcategory="显示"
+    export function showHex(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit((num >> 12) % 16, 0)
+        digit(num % 16, 3)
+        digit((num >> 4) % 16, 2)
+        digit((num >> 8) % 16, 1)
+    }
+
+    /**
+     * show Dot Point in given position
+     * @param bit is positiion, eg: 0
+     * @param show is true/false, eg: true
+     */
+    //% blockId="TM650_SHOW_DP" block="显示小数点在第 %bit|位，同时显示数字 %num"  group="TM1650数码管"
+    //% weight=80 blockGap=8
+	//% subcategory="显示"
+    export function showDpAt(bit: number, show: boolean) {
+        if (show) dat(bit, dbuf[bit % 4] | 0x80)
+        else dat(bit, dbuf[bit % 4] & 0x7F)
+    }
+
+    /**
+     * set display intensity
+     * @param dat is intensity of the display, eg: 3
+     */
+    //% blockId="TM650_INTENSITY" block="显示小数点在第 %dat位"  group="TM1650数码管"
+    //% weight=70 blockGap=8
+	//% subcategory="显示"
+    export function setIntensity(dat: number) {
+        if ((dat < 0) || (dat > 8))
+            return;
+        if (dat == 0)
+            off()
+        else {
+            _intensity = dat
+            cmd((dat << 4) | 0x01)
+        }
+    }
     
 
     /**
